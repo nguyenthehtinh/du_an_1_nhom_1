9a58f1b56cc45fe40a219cc794a9d3ffe09c19c9
Taanhh01 <hiendzml2301@gmail.com>
2025-08-02 11:41:02 +0700

    update code mới

--- commons/env.php
+++ commons/env.php
@@ -3,8 +3,10 @@
 // Biến môi trường, dùng chung toàn hệ thống
 // Khai báo dưới dạng HẰNG SỐ để không phải dùng $GLOBALS
 
-define('BASE_URL','http://localhost/Web-Thu-Cung-main/'); // Đường vào client
-define('BASE_URL_ADMIN', 'http://localhost/Web-Thu-Cung-main/admin/'); // Đường vào admin
+
+
+define('BASE_URL', 'http://localhost/Web-Thu-cung-main'); // Đường vào client
+define('BASE_URL_ADMIN', 'http://localhost/Web-Thu-cung-main/admin/'); // Đường vào admin
 
 define('DB_HOST', 'localhost');
 define('DB_PORT', 3306);
