8405c7db7c53dc6bf689272e22fcc02ce216471b
rine203 <protgas2003@gmail.com>
2025-08-02 12:09:12 +0700

    khanh hang

--- commons/env.php
+++ commons/env.php
@@ -3,13 +3,9 @@
 // Biến môi trường, dùng chung toàn hệ thống
 // Khai báo dưới dạng HẰNG SỐ để không phải dùng $GLOBALS
 
-<<<<<<< HEAD
+
 define('BASE_URL', 'http://localhost:8080/web-Thu-Cung-main/'); // Đường vào client
 define('BASE_URL_ADMIN', 'http://localhost:8080/Web-Thu-Cung-main/admin/'); // Đường vào admin
-=======
-define('BASE_URL', 'http://localhost/du_an_1_nhom_1/'); // Đường vào client
-define('BASE_URL_ADMIN', 'http://localhost/du_an_1_nhom_1/admin/'); // Đường vào admin
->>>>>>> 2d55ed0985b1e389453ec30f3c6b5a909a07a9b4
 
 define('DB_HOST', 'localhost');
 define('DB_PORT', 3306);
